a48cc7929a468d8f2a050e83f64a5cb8922d1673
zhang020801 <3115692761@qq.com>
2021-02-26 18:08:35 +0800

    完善supermall

--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67"><img src="~assets/img/home/recommend_bg.jpg" alt=""></a>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,7 @@
 </script>
 
 <style scoped>
-
+  .feature img{
+    width: 100%;
+  }
 </style>
